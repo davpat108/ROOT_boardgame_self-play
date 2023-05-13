4eb3f714efa265e266c1401de269e168dd005bcf
davpat108 <ddavidpatrikk@gmail.com>
2023-05-13 15:38:09 +0200

    ISMCTS sample generation added

--- ISMCTS.py
+++ ISMCTS.py
@@ -22,32 +22,32 @@ class Set:
         """
 
         # Card I'm not seeing = deck+hands together
-        avalible_quest_deck = copy(self.game_copy.deck.cards)
+        avalible_deck = self.game_copy.deck.cards
         for player in [self.game_copy.marquise, self.game_copy.eyrie, self.game.alliance, self.game.vagabond]:
             if player.name != self.player:
-                avalible_quest_deck.cards += player.deck.cards
+                avalible_deck.cards += player.deck.cards
         
         if self.player != "Alliance":
-            avalible_quest_deck.cards += self.game_copy.alliance.supporter_deck.cards
+            avalible_deck.cards += self.game_copy.alliance.supporter_deck.cards
 
-        avalible_quest_deck.random.shuffle()
+        avalible_deck.random.shuffle()
 
         for player in [self.game_copy.marquise, self.game_copy.eyrie, self.game.alliance, self.game.vagabond]:
             if player.name != self.player:
                 prev_hand_size = len(player.deck.cards)
                 player.deck = Deck(empty=True)
                 for _ in range(prev_hand_size):
-                    player.deck.add_card(avalible_quest_deck.draw_card())
+                    player.deck.add_card(avalible_deck.draw_card())
         
         if self.player != "Alliance":
             prev_supporter_size = len(self.game_copy.alliance.supporter_deck.cards)
             self.game_copy.alliance.supporter_deck = Deck(empty=True)
             for _ in range(prev_supporter_size):
-                self.game_copy.alliance.supporter_deck.add_card(avalible_quest_deck.draw_card())
+                self.game_copy.alliance.supporter_deck.add_card(avalible_deck.draw_card())
         
 
         if self.player != "Vagabond":
-            avalible_quest_deck = copy(self.game_copy.quest_deck.cards)
+            avalible_quest_deck = self.game_copy.quest_deck.cards
             avalible_quest_deck.random.shuffle()
             vagabond_quest_deck_size = len(self.game_copy.vagabond.quest_deck.cards)
             self.game_copy.vagabond.quest_deck = QuestDeck(empty=True)
@@ -55,8 +55,53 @@ class Set:
                 self.game_copy.vagabond.quest_deck.add_card(avalible_quest_deck.draw_card())
 
     
-    def generate_samples_with_prior_samples(self):
+    def generate_samples_someone_draws_a_card(self, player):
         """
-        Generate samples knowing the previous random samples
+        Generate samples knowing the previous random samples, and someone draws a card
+        """
+        pass
+
+
+    def generate_samples_one_player_gives_a_card_to_another(self, player1, player2, to_supporter_deck=False):
+        """
+        Generate samples knowing the previous random samples and a player gives a card to another player
+        """
+        if not to_supporter_deck:
+            avalible_deck = self.game_copy.deck.cards
+            for player in [self.game_copy.marquise, self.game_copy.eyrie, self.game.alliance, self.game.vagabond]:
+                if player.name == player1 or player.name == player2:
+                    avalible_deck.cards += player.deck.cards
+            avalible_deck.random.shuffle()
+            for player in [self.game_copy.marquise, self.game_copy.eyrie, self.game.alliance, self.game.vagabond]:
+                if player.name == player1 or player.name == player2:
+                    prev_hand_size = len(player.deck.cards)
+                    player.deck = Deck(empty=True)
+                    for _ in range(prev_hand_size):
+                        player.deck.add_card(avalible_deck.draw_card())
+        
+        else:
+            if not player2 == "Alliance":
+                raise Exception("Can't give to supporter deck if not alliance")
+            avalible_deck = self.game_copy.deck.cards
+
+            for player in [self.game_copy.marquise, self.game_copy.eyrie, self.game.vagabond]:
+                if player.name == player1:
+                    avalible_deck.cards += player.deck.cards
+            avalible_deck.cards += self.game_copy.alliance.supporter_deck.cards
+            avalible_deck.random.shuffle()
+            for player in [self.game_copy.marquise, self.game_copy.eyrie, self.game.vagabond]:
+                if player.name == player1:
+                    prev_hand_size = len(player.deck.cards)
+                    player.deck = Deck(empty=True)
+                    for _ in range(prev_hand_size):
+                        player.deck.add_card(avalible_deck.draw_card())
+            prev_supporter_size = len(self.game_copy.alliance.supporter_deck.cards)
+            self.game_copy.alliance.supporter_deck = Deck(empty=True)
+            for _ in range(prev_supporter_size):
+                self.game_copy.alliance.supporter_deck.add_card(avalible_deck.draw_card())
+    
+    def generate_samples_I_gave_a_card_to_another_payer(self, player):
+        """
+        Propably unncessary as I know their whole hand at this point
         """
         pass
--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # ROOT_boardgame_self-play
-Implementation of the popular boardgame ROOT, and training a self play AI on it
+Implementation of the popular boardgame ROOT, and training a self play AI on it. The game backbone is made out of 3 parts. The first part contains all the info that represents the gamestate (map.py, actors.py), the secound all the possible actions in that gamestate (actors.py), the third carries out the options and changes the gamestate (game.py).
 
+# Progess
+Game backbone is mostly done, currently trying a information set monte carlo tree search. 
+After ISMCTS is done, I'll use the models and encodings that are already written in model.py and test_model.py to implement more advanced solutions.
 ## The map
 The map is stored as a graph, where each node is a place (meaning a clearing or a forest).
 
@@ -9,7 +12,6 @@ The common deck and the vagabonds quest deck is implemented here.
 
 ## Actors
 The 4 actors and the actions they can take from the state of the map, their board and their hand.
-
 
 ## Game
 Functions that carry out the options from actors are located here
@@ -22,12 +24,10 @@ The bigger constants are located here.
 
 ## DTOS
 Data transfer objects between options in the game state and functions that carry out the options.
-## TODO
-
-tax collection
-something with alliance soldiers
-
+## Errors I couldn't figure out yet
+Vagabond around once every 4000 fully random games tries to exhaust 2 boots while going to a hostile clearing, while only having one. Get_moves shouldn't return this move possibility. I can't figure out why it does.
 
 # TODO first gonna skip
 which woods to use when building, if you can build by the rule supply lines, you might be able to build unsupported wood
 which craft activations to use for royal claim
+tax collection throughout daylight instead of only at the start
